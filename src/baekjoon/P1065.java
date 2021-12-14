d56950fd154215d918e4f54ec28a4639b22e4ac0
KIM-KYOUNG-OH <66231761+KIM-KYOUNG-OH@users.noreply.github.com>
2021-12-15 01:01:27 +0900

    알고리즘풀이

--- src/baekjoon/P1065.java
+++ src/baekjoon/P1065.java
@@ -1,5 +1,38 @@
 package baekjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class P1065 {
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(arithmeticSequence(Integer.parseInt(br.readLine())));
+    }
+
+    public static int arithmeticSequence(int num) {
+        int cnt = 0;
+
+        if(num < 100) {
+            return num;
+        }
+
+        cnt = 99;
+        if (num == 1000) {
+            num = 999;
+        }
+
+        for(int i = 100; i <= num; i++){
+            int first = i / 100;
+            int second = (i / 10) % 10;
+            int third = i % 10;
+
+            if((first - second) == (second - third)) {
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
 }
