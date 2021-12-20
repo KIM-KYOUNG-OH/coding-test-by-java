d33a67ddc8ea72677abf505b4b36935adbc28484
KIM-KYOUNG-OH <66231761+KIM-KYOUNG-OH@users.noreply.github.com>
2021-12-21 01:44:07 +0900

    .

---
+++ src/programmers/numberStringAndAlphabet.java
@@ -0,0 +1,14 @@
+package programmers;
+
+public class numberStringAndAlphabet {
+    public int solution(String s) {
+        String[] digits = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+        String[] alphabets = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+        for(int i = 0; i < 10; i++) {
+            s = s.replace(alphabets[i], digits[i]);
+        }
+
+        return Integer.parseInt(s);
+    }
+}
