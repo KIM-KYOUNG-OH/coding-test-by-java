717d60c349a1a0e5464d82e932f0af3f4eb2d818
KIM-KYOUNG-OH <66231761+KIM-KYOUNG-OH@users.noreply.github.com>
2022-01-05 23:11:46 +0900

    .

--- src/programmers/greedy/ConnectIsland.java
+++ src/programmers/greedy/ConnectIsland.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 /**
  * 1차 : solution 참고
+ * 2차 : union-find 로직 이해부족
  */
 public class ConnectIsland {
     // Minimum Spanning Tree : 최소 신장 트리
@@ -40,14 +41,16 @@ public class ConnectIsland {
             int to = edge[1];
             int cost = edge[2];
 
-            int fromParent = findParent(from);
-            int toParent = findParent(to);
+            int fromParent = findAndSetParent(from);
+            int toParent = findAndSetParent(to);
 
             // 부모노드가 같다면 해당 간선을 선택하지 않는다.
             if(fromParent == toParent) continue;
 
             answer += cost;
+
             // 간선을 연결해 두 노드가 같은 그래프에 속하게 되었으므로 부모 노드를 갱신
+            // 더 작은 값을 root로 지정
             parent[toParent] = Math.min(fromParent, toParent);
             parent[fromParent] = Math.min(fromParent, toParent);
         }
@@ -56,8 +59,10 @@ public class ConnectIsland {
     }
 
     // 부모노드가 자기자신과 같은 노드를 찾을 때까지 깊이 탐색
-    private int findParent(int node) {
+    // 부모노드 조회와 동시에 같은 그래프에 속한 노드들 root값 수정(재귀이므로 root부터 node까지 차례로 수정됨)
+    private int findAndSetParent(int node) {
         if(parent[node] == node) return node;
-        return parent[node] = findParent(parent[node]);
+        return parent[node] = findAndSetParent(parent[node]);
     }
+
 }
--- src/programmers/greedy/SpeedCamera.java
+++ src/programmers/greedy/SpeedCamera.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 /**
  * 1차 : solution 참고
+ * 2차 : 통과, 근데 다음에 풀 때도 그리드 알고리즘을 생각해낼 수 있을지 미지수
  */
 public class SpeedCamera {
     public int solution(int[][] routes) {
