fb396262a4f99580104933073a30af55dbf1188f
KIM-KYOUNG-OH <66231761+KIM-KYOUNG-OH@users.noreply.github.com>
2022-04-05 00:08:55 +0900

    .

---
+++ src/programmers/level2/VowelsDictionary.java
@@ -0,0 +1,17 @@
+package programmers.level2;
+
+public class VowelsDictionary {
+    public int solution(String word) {
+        int answer = word.length();
+        char[] alps = new char[]{'A', 'E', 'I', 'O', 'U'};
+        int[] num = new int[]{781, 156, 31, 6, 1};
+        for(int i = 0; i < word.length(); i++) {
+            for(int j = 0; j < 5; j++) {
+                if(word.charAt(i) == alps[j]) {
+                    answer += num[i] * j;
+                }
+            }
+        }
+        return answer;
+    }
+}
