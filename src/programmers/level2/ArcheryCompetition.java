4c6382ee719427e9712cb38071e3ebe6e549e8d8
KIM-KYOUNG-OH <kdkg0434@gmail.com>
2022-10-25 10:53:03 +0900

    10/24

--- src/programmers/level2/ArcheryCompetition.java
+++ src/programmers/level2/ArcheryCompetition.java
@@ -51,6 +51,54 @@ public class ArcheryCompetition {
 //        }
 //    }
 
+//    static int max;
+//    static int[] answer;
+//    static int[] lion;
+//
+//    public int[] solution(int n, int[] info) {
+//        max = 0;
+//        answer = new int[]{-1};
+//        lion = new int[11];
+//        dfs(n, info, 0);
+//
+//        return answer;
+//    }
+//
+//    private void dfs(int n, int[] apeach, int cnt) {
+//        if(cnt == n) {
+//            int apeachScore = 0;
+//            int lionScore = 0;
+//            for(int i = 0; i < 11; i++) {
+//                if(apeach[i] == 0 && lion[i] == 0) continue;
+//
+//                if(apeach[i] >= lion[i]) {
+//                    apeachScore += 10 - i;
+//                } else {
+//                    lionScore += 10 - i;
+//                }
+//            }
+//
+//            if(apeachScore < lionScore) {
+//                if(lionScore - apeachScore >= max) {
+//                    max = lionScore - apeachScore;
+//                    answer = lion.clone();
+//                }
+//            }
+//
+//            return;
+//        }
+//
+//        for(int i = 0; i < 11; i++) {
+//            if(lion[i] <= apeach[i]) {
+//                lion[i]++;
+//                dfs(n, apeach, cnt + 1);
+//                lion[i]--;
+//            } else {
+//                break;
+//            }
+//        }
+//    }
+
     static int max;
     static int[] answer;
     static int[] lion;
@@ -59,6 +107,7 @@ public class ArcheryCompetition {
         max = 0;
         answer = new int[]{-1};
         lion = new int[11];
+
         dfs(n, info, 0);
 
         return answer;
@@ -89,7 +138,7 @@ public class ArcheryCompetition {
         }
 
         for(int i = 0; i < 11; i++) {
-            if(lion[i] <= apeach[i]) {
+            if(apeach[i] >= lion[i]) {
                 lion[i]++;
                 dfs(n, apeach, cnt + 1);
                 lion[i]--;
--- src/programmers/level2/TriangleSnail.java
+++ src/programmers/level2/TriangleSnail.java
@@ -1,47 +1,86 @@
 package programmers.level2;
 
 public class TriangleSnail {
+//    public int[] solution(int n) {
+//        int[] answer = new int[n * (n + 1) / 2];
+//
+//        boolean[][] visited = new boolean[n + 1][n];
+//        int[][] matrix = new int[n + 1][n];
+//        visited[0][0] = true;
+//        int y = 0, x = 0;
+//        int cur = 1;
+//        int[] dy = {1, 0, -1};
+//        int[] dx = {0, 1, -1};
+//
+//        while(n > 0) {
+//            for(int i = 0; i < 3; i++) {
+//                boolean breaker = false;
+//                for(int j = 0; j < n; j++) {
+//                    int ny = y + dy[i];
+//                    int nx = x + dx[i];
+//
+//                    if(visited[ny][nx]) {
+//                        breaker = true;
+//                        break;
+//                    }
+//
+//                    matrix[ny][nx] = cur;
+//                    visited[ny][nx] = true;
+//                    cur++;
+//                }
+//
+//                n--;
+//                if (breaker) break;
+//            }
+//        }
+//
+//        int idx = 0;
+//        for(int i = 1; i < n; i++) {
+//            for(int j = 0; j < i; j++) {
+//                answer[idx] = matrix[i][j];
+//                idx++;
+//            }
+//        }
+//
+//        return answer;
+//    }
+
+    static int[] dy = {1, 0, -1};
+    static int[] dx = {0, 1, -1};
+
     public int[] solution(int n) {
-        int[] answer = new int[n * (n + 1) / 2];
-
-        boolean[][] visited = new boolean[n + 1][n];
-        int[][] matrix = new int[n + 1][n];
-        visited[0][0] = true;
-        int y = 0, x = 0;
-        int cur = 1;
-        int[] dy = {1, 0, -1};
-        int[] dx = {0, 1, -1};
-
-        while(n > 0) {
-            for(int i = 0; i < 3; i++) {
-                boolean breaker = false;
-                for(int j = 0; j < n; j++) {
-                    int ny = y + dy[i];
-                    int nx = x + dx[i];
-
-                    if(visited[ny][nx]) {
-                        breaker = true;
-                        break;
-                    }
-
-                    matrix[ny][nx] = cur;
-                    visited[ny][nx] = true;
-                    cur++;
-                }
-
-                n--;
-                if (breaker) break;
+        int[][] matrix = new int[n][n];
+        int startY = -1;
+        int startX = 0;
+        int depth = n;
+        int direction = 0;
+        int seq = 1;
+        while(depth > 0) {
+            for(int i = 0; i < depth; i++) {
+                int ny = startY + (i + 1) * dy[direction];
+                int nx = startX + (i + 1) * dx[direction];
+                matrix[ny][nx] = seq++;
             }
+
+            startY += depth * dy[direction];
+            startX += depth * dx[direction];
+            direction = (direction + 1) % 3;
+            depth--;
         }
 
+        int[] answer = new int[(n * (n + 1)) / 2];
         int idx = 0;
-        for(int i = 1; i < n; i++) {
-            for(int j = 0; j < i; j++) {
-                answer[idx] = matrix[i][j];
-                idx++;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j <= i; j++) {
+                answer[idx++] = matrix[i][j];
             }
         }
 
         return answer;
+    }
+
+    public static void main(String[] args) {
+        TriangleSnail s = new TriangleSnail();
+        s.solution(6);
     }
 }
