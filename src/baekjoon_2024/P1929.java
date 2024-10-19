d8e1431b53746e66b587972e85e187fbea0419ea
kdkg0 <kdkg0434@gmail.com>
2024-10-20 00:34:49 +0900

    .

--- src/baekjoon_2024/P1929.java
+++ src/baekjoon_2024/P1929.java
@@ -16,16 +16,15 @@ public class P1929 {
             isPrime[i] = true;
         }
 
-        for (int i = 2; i <= n / 2; i++) {
-            for (int j = 2; j <= n / i; j++) {
-                isPrime[i * j] = false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (!isPrime[i]) continue;
+            for (int j = i * i; j <= n; j += i) {
+                isPrime[j] = false;
             }
         }
 
         for (int i = m; i <= n; i++) {
-            if (isPrime[i]) {
-                bw.write(i + "\n");
-            }
+            if (isPrime[i]) bw.write(i + "\n");
         }
 
         bw.close();
