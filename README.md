77c93b23966aa13c59a344593139a138165a2557
kim-kyoung-oh <kdkg0434@gmail.com>
2024-11-03 23:30:28 +0900

    .

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
 # coding-test-by-java
 
+# 알고리즘 유형
+- brute force
+- greedy
+- bfs/dfs
+- dynamic programming
+- 해시
+- 정렬
+- 이진 탐색
+- 하노이 문제
+- 에라토스테네스의 체
+- knapsack
+  - Fraction Knapsack Problem
+  - 0/1 Knapsack Problem
+
 # 솔루션 보고 푼 문제
 - 백준
   - P1931
   - P1912
   - P9663
+  - P11729
--- src/baekjoon_2024/P11729.java
+++ src/baekjoon_2024/P11729.java
@@ -1,7 +1,40 @@
 package baekjoon_2024;
 
-public class P11729 {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class P11729 {
+    private static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        sb.append((int) Math.pow(2, n) - 1);
+        sb.append("\n");
+
+        hanoi(n, 1, 2, 3);
+        bw.write(sb.toString());
+
+        bw.close();
+        br.close();
+    }
+
+    private static void hanoi(int n, int start, int mid, int end) {
+        if (n == 1) {
+            sb.append(start);
+            sb.append(" ");
+            sb.append(end);
+            sb.append("\n");
+            return;
+        }
+
+        hanoi(n - 1, start, end, mid);
+
+        sb.append(start);
+        sb.append(" ");
+        sb.append(end);
+        sb.append("\n");
+
+        hanoi(n - 1, mid, start, end);
     }
 }
