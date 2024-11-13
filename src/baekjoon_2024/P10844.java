ac2405cc59c026af59b186020c1724d527b618fa
kim-kyoung-oh <kdkg0434@gmail.com>
2024-11-13 23:53:25 +0900

    .

--- src/baekjoon_2024/P10844.java
+++ src/baekjoon_2024/P10844.java
@@ -3,25 +3,15 @@ package baekjoon_2024;
 import java.io.*;
 import java.util.Deque;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.Set;
 
 public class P10844 {
-    private static Set<String> set = new HashSet<>();
-    private static long answer;
     private static int n;
-    private static Deque<Integer> deque;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         n = Integer.parseInt(br.readLine());
-        deque = new LinkedList<>();
-        answer = 0;
-
-        recursive(0);
-
-        bw.write(String.valueOf(answer % 1000000000));
 
         bw.close();
         br.close();
