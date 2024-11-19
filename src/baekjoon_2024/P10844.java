2569a1262e0654b0ca2d97cd54e63fb0323f163f
kim-kyoung-oh <kdkg0434@gmail.com>
2024-11-19 23:20:27 +0900

    .

--- src/baekjoon_2024/P10844.java
+++ src/baekjoon_2024/P10844.java
@@ -12,7 +12,6 @@ public class P10844 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         n = Integer.parseInt(br.readLine());
-
         bw.close();
         br.close();
     }
