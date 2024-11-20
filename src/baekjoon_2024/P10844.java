d7cd4a16485fa714ca2e8b9b453eb19eae3019fd
kim-kyoung-oh <kdkg0434@gmail.com>
2024-11-20 23:20:26 +0900

    .

--- src/baekjoon_2024/P10844.java
+++ src/baekjoon_2024/P10844.java
@@ -1,9 +1,6 @@
 package baekjoon_2024;
 
 import java.io.*;
-import java.util.Deque;
-import java.util.HashSet;
-import java.util.Set;
 
 public class P10844 {
     private static int n;
@@ -12,6 +9,7 @@ public class P10844 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         n = Integer.parseInt(br.readLine());
+
         bw.close();
         br.close();
     }
